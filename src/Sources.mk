f978603a5cdeb3c5d438ce27dace2c8e45fe4f1c
Turupawn <ahmed.hn.43@gmail.com>
2017-08-23 15:59:07 -0600

    separate sources file to be share between all platform outputs

---
+++ src/Sources.mk
@@ -0,0 +1,3 @@
+MODWORKS_WRAPPERS=CurlWrapper.cpp MinizipWrapper.cpp
+MODWORKS_DEPENDENCIES=minizip/unzip.cpp minizip/ioapi.cpp
+MODWORKS_SOURCES=ModworksSDK.cpp Utility.cpp Mod.cpp $(MODWORKS_WRAPPERS) $(MODWORKS_DEPENDENCIES)
