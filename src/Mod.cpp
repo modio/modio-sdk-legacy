bbb1c6d78cc93845c8f9024c63acd23c37df4308
Turupawn <ahmed.hn.43@gmail.com>
2017-09-24 21:54:31 -0600

    mod nested object retreival

--- src/Mod.cpp
+++ src/Mod.cpp
@@ -16,6 +16,78 @@ namespace modworks
     return logo;
   }
 
+  Modfile jsonToModfile(json modfile_json)
+  {
+    Modfile modfile;
+    if(hasKey(modfile_json, "id"))
+      modfile.id = modfile_json["id"];
+    if(hasKey(modfile_json, "mod"))
+      modfile.mod = modfile_json["mod"];
+    if(hasKey(modfile_json, "member"))
+      modfile.member = modfile_json["member"];
+    if(hasKey(modfile_json, "datevirus"))
+      modfile.datevirus = modfile_json["datevirus"];
+    if(hasKey(modfile_json, "virusstatus"))
+      modfile.virusstatus = modfile_json["virusstatus"];
+    if(hasKey(modfile_json, "viruspoitive"))
+      modfile.viruspoitive = modfile_json["viruspoitive"];
+    if(hasKey(modfile_json, "filesize"))
+      modfile.filesize = modfile_json["filesize"];
+    if(hasKey(modfile_json, "filehash"))
+      modfile.filehash = modfile_json["filehash"];
+    if(hasKey(modfile_json, "filename"))
+      modfile.filename = modfile_json["filename"];
+    if(hasKey(modfile_json, "version"))
+      modfile.version = modfile_json["version"];
+    //virustotal ?
+    if(hasKey(modfile_json, "changelog"))
+      modfile.changelog = modfile_json["changelog"];
+    if(hasKey(modfile_json, "download"))
+      modfile.download = modfile_json["download"];
+
+    return modfile;
+  }
+
+  Ratings jsonToRatings(json ratings_json)
+  {
+    Ratings ratings;
+
+    if(hasKey(ratings_json, "total"))
+      ratings.total = ratings_json["total"];
+    if(hasKey(ratings_json, "positive"))
+      ratings.positive = ratings_json["positive"];
+    if(hasKey(ratings_json, "negative"))
+      ratings.negative = ratings_json["negative"];
+    if(hasKey(ratings_json, "weighted"))
+      ratings.weighted = ratings_json["weighted"];
+    if(hasKey(ratings_json, "percentage"))
+      ratings.percentage = ratings_json["percentage"];
+    if(hasKey(ratings_json, "stars"))
+      ratings.stars = ratings_json["stars"];
+    if(hasKey(ratings_json, "text"))
+      ratings.text = ratings_json["text"];
+
+    return ratings;
+  }
+
+  Tag jsonToTag(json tag_json)
+  {
+    Tag tag;
+
+    if(hasKey(tag_json, "game"))
+      tag.game = tag_json["game"];
+    if(hasKey(tag_json, "mod"))
+      tag.mod = tag_json["mod"];
+    if(hasKey(tag_json, "date"))
+      tag.date = tag_json["date"];
+    if(hasKey(tag_json, "member"))
+      tag.member = tag_json["member"];
+    if(hasKey(tag_json, "tag"))
+      tag.tag = tag_json["tag"];
+
+    return tag;
+  }
+
   Mod* jsonToMod(json mod_json)
   {
     Mod* mod = new Mod;
@@ -50,18 +122,9 @@ namespace modworks
     //metadata ?
     //media ?
 
-    Modfile modfile;
-    Ratings ratings;
-    Tag tag;
-
-    if(hasKey(mod_json, "modfile"))
-    {
-      json modfile_json = mod_json["modfile"];
-      if(hasKey(modfile_json, "download"))
-      {
-        mod->modfile.download = modfile_json["download"];
-      }
-    }
+    mod->modfile = jsonToModfile(mod_json["modfile"]);
+    mod->ratings = jsonToRatings(mod_json["ratings"]);
+    mod->tag = jsonToTag(mod_json["tag"]);
 
     return mod;
   }
