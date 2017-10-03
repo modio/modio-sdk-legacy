9bb7ee68bc686228855b00c61be7ffc19ab25b03
Turupawn <ahmed.hn.43@gmail.com>
2017-10-02 23:03:41 -0600

    cleaning up example

--- examples/Example.cpp
+++ examples/Example.cpp
@@ -12,17 +12,9 @@
 #endif
 
 int files_downloaded = 0;
-int files_to_download = 50;
+int files_to_download = 5;
 bool email_request_finished = false;
 bool email_exchange_finished = false;
-
-void onModAdded(int response_code, modworks::Mod* mod)
-{
-  if(response_code == 200)
-  {
-    cout<<"Mod added with id: "<<mod->id<<endl;
-  }
-}
 
 void onEmailRequest(int response_code)
 {
@@ -48,7 +40,7 @@ void onExchange(int response_code)
   email_exchange_finished = true;
 }
 
-void onThumbnailDownloaded(int response_code,  modworks::Mod* mod, string path)
+void onImageDownloaded(int response_code,  modworks::Mod* mod, string path)
 {
   if(response_code == 200)
   {
@@ -74,6 +66,7 @@ void onMediaImagesDownloaded(int response_code, modworks::Mod* mod, vector<strin
     for(int i=0;i<(int)images.size();i++)
       cout<<images[i]<<endl;
   }
+  files_downloaded++;
 }
 
 void onModsGet(int status, vector<modworks::Mod*> mods)
@@ -91,26 +84,11 @@ void onModsGet(int status, vector<modworks::Mod*> mods)
   cout<<"=================="<<endl;
   for(int i=0;i<(int)mods.size();i++)
   {
-    modworks::downloadModLogoThumbnail(mods[i], &onThumbnailDownloaded);
-    modworks::downloadModLogoFull(mods[i], &onThumbnailDownloaded);
+    modworks::downloadModLogoThumbnail(mods[i], &onImageDownloaded);
+    modworks::downloadModLogoFull(mods[i], &onImageDownloaded);
     modworks::downloadModMediaImagesThumbnail(mods[i], &onMediaImagesDownloaded);
     modworks::downloadModMediaImagesFull(mods[i], &onMediaImagesDownloaded);
     modworks::installMod(mods[i], "mod_directory/"+mods[i]->name,&onModInstalled);
-  }
-}
-
-void printProgress()
-{
-  while(files_downloaded<files_to_download)
-  {
-    if(modworks::getCurrentDownloadInfo().url != "")
-    {
-      double percentage = 0;
-      if(modworks::getCurrentDownloadInfo().download_progress != 0 && modworks::getCurrentDownloadInfo().download_total != 0)
-        percentage = 100.0 * modworks::getCurrentDownloadInfo().download_progress / modworks::getCurrentDownloadInfo().download_total;
-      cout<<percentage<<"%"<<endl;
-      sleep(1);
-    }
   }
 }
 
@@ -134,34 +112,15 @@ int main(void)
     while(!email_exchange_finished);
   }
 
-/*
-  mworks->addMod( "test22",//Mod params
-                  "http://hello.com",
-                  "test test test test test test test test test test test test test test test",
-                  "logo.png",
-                  "dir/moddir",//File params
-                  "0.0.1",
-                  "This is a changelog text",
-                  &onModAdded);//Callback
-*/
-
   modworks::Filter* filter = new modworks::Filter;
   modworks::addFilterInField(filter,"id","31");
   modworks::getMods(filter, &onModsGet);
 
-  while(true);
+  while(files_downloaded<files_to_download);
 
-/*
-  int x;
-  cin>>x;
-
-  modworks::pauseCurrentDownload();
-
-  cin>>x;
-
-  cout<<"Process finished!"<<endl;
-*/
   modworks::shutdown();
+
+  cout<<"Process finished"<<endl;
 
   return 0;
 }
