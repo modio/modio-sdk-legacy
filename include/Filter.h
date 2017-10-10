6196cc86fe348173a6464f5eebc6f8ed2214b976
Turupawn <ahmed.hn.43@gmail.com>
2017-10-10 12:12:58 -0700

    visual c++ x64 support

--- include/Filter.h
+++ include/Filter.h
@@ -7,6 +7,16 @@
 
 using namespace std;
 
+#ifdef WINDOWS
+#  ifdef BUILDING_MODIO_DLL
+#    define MODIO_DLL __declspec(dllexport)
+#  else
+#    define MODIO_DLL __declspec(dllimport)
+#  endif
+#else
+#  define MODIO_DLL
+#endif
+
 namespace modio
 {
   struct Filter
@@ -16,20 +26,20 @@ namespace modio
 
   string getFilterString(Filter* filter);
 
-  void setFilterSort(Filter* filter, string field, bool ascending);
-  void setFilterLimit(Filter* filter, int limit);
-  void setFilterOffset(Filter* filter, int offset);
-  void setFilterCursor(Filter* filter, int cursor);
-  void setFilterFullTextSearch(Filter* filter, string text);
-  void addFilterLikeField(Filter* filter, string field, string value);
-  void addFilterNotLikeField(Filter* filter, string field, string value);
-  void addFilterInField(Filter* filter, string field, string value);
-  void addFilterNotInField(Filter* filter, string field, string value);
-  void addFilterMinField(Filter* filter, string field, double value);
-  void addFilterMaxField(Filter* filter, string field, double value);
-  void addFilterSmallerThanField(Filter* filter, string field, double value);
-  void addFilterGreaterThanField(Filter* filter, string field, double value);
-  void addFilterNotEqualField(Filter* filter, string field, string value);
+  void MODIO_DLL setFilterSort(Filter* filter, string field, bool ascending);
+  void MODIO_DLL setFilterLimit(Filter* filter, int limit);
+  void MODIO_DLL setFilterOffset(Filter* filter, int offset);
+  void MODIO_DLL setFilterCursor(Filter* filter, int cursor);
+  void MODIO_DLL setFilterFullTextSearch(Filter* filter, string text);
+  void MODIO_DLL addFilterLikeField(Filter* filter, string field, string value);
+  void MODIO_DLL addFilterNotLikeField(Filter* filter, string field, string value);
+  void MODIO_DLL addFilterInField(Filter* filter, string field, string value);
+  void MODIO_DLL addFilterNotInField(Filter* filter, string field, string value);
+  void MODIO_DLL addFilterMinField(Filter* filter, string field, double value);
+  void MODIO_DLL addFilterMaxField(Filter* filter, string field, double value);
+  void MODIO_DLL addFilterSmallerThanField(Filter* filter, string field, double value);
+  void MODIO_DLL addFilterGreaterThanField(Filter* filter, string field, double value);
+  void MODIO_DLL addFilterNotEqualField(Filter* filter, string field, string value);
 }
 
 #endif
--- include/handlers/ModHandler.h
+++ include/handlers/ModHandler.h
@@ -3,6 +3,16 @@
 
 #include "Globals.h"
 
+#ifdef WINDOWS
+#  ifdef BUILDING_MODIO_DLL
+#    define MODIO_DLL __declspec(dllexport)
+#  else
+#    define MODIO_DLL __declspec(dllimport)
+#  endif
+#else
+#  define MODIO_DLL
+#endif
+
 namespace modio
 {
   class ModHandler
@@ -10,16 +20,16 @@ namespace modio
   public:
     map<string, string> curlform_copycontents;
     map<string, string> curlform_files;
-    void setLogoPath(string logo_path);
-    void setName(string name);
-    void setHomepage(string homepage);
-    void setSummary(string summary);
-    void setPrice(double price);
-    void setStock(int stock);
-    void setDescription(string description);
-    void setMetadata(string metadata);
-    void setNameid(string nameid);
-    void setModfile(int modfile);
+    void MODIO_DLL setLogoPath(string logo_path);
+    void MODIO_DLL setName(string name);
+    void MODIO_DLL setHomepage(string homepage);
+    void MODIO_DLL setSummary(string summary);
+    void MODIO_DLL setPrice(double price);
+    void MODIO_DLL setStock(int stock);
+    void MODIO_DLL setDescription(string description);
+    void MODIO_DLL setMetadata(string metadata);
+    void MODIO_DLL setNameid(string nameid);
+    void MODIO_DLL setModfile(int modfile);
   };
 }
 
