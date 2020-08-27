c5c9368610d3afc10f4f5f80c8bc3c59eccf7e00
Markus Rännare <mackyman@gmail.com>
2020-08-27 14:37:06 +0200

    Cleaned up Filesystem.h

--- src/Filesystem.h
+++ src/Filesystem.h
@@ -4,21 +4,10 @@
 #include "c/ModioC.h"
 #include <string>
 
-// @todonow: Move this when moving WideChar...
-#include <codecvt>
-
-namespace modio
-{
-  struct GenericFilesystem
-  {  
-  };
-}
-
 #if defined(MODIO_WINDOWS_DETECTED)
+#include <codecvt>
 namespace modio
 {
-  // @todonow: Migrate this to WindowsFilesystem.cpp
-                               // for wstring_convert/codecvt_utf8
   static std::wstring WideCharFromString(std::string const& str)
   {
     return std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(str);
@@ -34,7 +23,6 @@ namespace modio
     return std::wstring_convert<std::codecvt_utf8<wchar_t>>().to_bytes(str);
   }
 }
-#elif defined(MODIO_LINUX_DETECTED) || defined(MODIO_OSX_DETECTED)
 #endif
 
 #endif
